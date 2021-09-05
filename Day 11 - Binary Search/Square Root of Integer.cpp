d9e71671c84b500fcdfc5b9ee56f8a3f8623fd31
Vikalp Chakravorty <vicks548@gmail.com>
2021-09-05 11:48:37 +0530

    Striver SDE Sheet

---
+++ Day 11 - Binary Search/Square Root of Integer.cpp
@@ -0,0 +1,24 @@
+long long binarySearch(int x) {
+    long long s = 2, e = x/2+1, mid;        
+    while(s <= e) {
+        mid= s + (e-s)/2;
+        if(mid*mid == x || mid*mid<x && (mid+1)*(mid+1)>x) {
+            break;
+        } else if(mid*mid>x) {
+            e = mid-1;
+        } else {
+            s = mid+1;
+        } 
+    }
+    return mid;
+}
+
+int Solution::sqrt(int A) {
+    if(A == 0) {
+        return 0;
+    }
+    if(A < 4) {
+        return 1;
+    }
+    return binarySearch(A);
+}
